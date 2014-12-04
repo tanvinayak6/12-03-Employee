7706485669074544ab8d0272c6be550ae4fdb73f
tanvinayak6 <tncces1@gmail.com>
2014-12-03 17:45:06 -0700

    Update Employee.java

--- src/Employee.java
+++ src/Employee.java
@@ -1,1 +1,55 @@
 // placeholder for your Employee class
+// placeholder for your Employee class
+public class Employee
+{
+  String name;
+  double wage;
+  int hours;
+  public Employee(String newname, double newwage, int newhours)
+  {
+    name=newname;
+    hours=newhours;
+    wage=newhours;
+  }
+  public String getName()
+  {
+    return name;
+  }
+  public double getWage()
+  {
+    return wage;
+  }
+  public int getHours()
+  {
+    return hours;
+  }
+  public double getWeeklySalary()
+  {
+    return wage*hours;
+  }
+  public double getAnnualSalary()
+  {
+    return 52*hours*wage;
+  }
+  public String toString()
+  {
+    return "name: "+name+" , hours: "+hours+" , wage: "+wage;
+  }
+  public void setName( String newname)
+  {
+    name=newname;
+    
+  }
+  public void setWage(double newwage)
+  {
+    wage=newwage;
+  }
+  public void setHours(int newhours)
+  {
+    hours=newhours;
+  }
+  public void giveRaise()
+  {
+    wage+=1.0;
+  }
+}
